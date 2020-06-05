52c18368e0eb7944cb5b0589c80a8f602692313a
Ricardo Carvalho Santos <42756551+RicardoEstudante@users.noreply.github.com>
2020-06-05 16:01:12 -0300

    start front

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <h1>Hello world</h1>    
-  );
-}
+import Home from './pages/Home';
+
+function App(){
+   return(
+     <Home />
+   )
+ }
 
 export default App;
