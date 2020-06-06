322d7993a0f13a70950ae1fc6f14461cc50efa3d
Ricardo Carvalho Santos <42756551+RicardoEstudante@users.noreply.github.com>
2020-06-06 01:10:54 -0300

    add map

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer, Marker } from 'react-leaflet';
 
 import './styles.css'
 
 import logo from '../../assets/logo.svg';
+import { tileLayer } from 'leaflet';
 
 const CreatePoint = () =>{
     return (
@@ -60,6 +62,13 @@ const CreatePoint = () =>{
                         <h2>Endereço</h2>
                         <span>Selecione o endereço no mapa</span>
                     </legend>
+
+                    <Map center={[-23.2691466, -46.7381793]} zoom={15}>
+                        <TileLayer
+                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                    </Map>
 
                     <div className="field-group">
                         <div className="field">
