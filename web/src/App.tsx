ff594045d08e155020fa6abb9adf020b068f4ddf
Ricardo Carvalho Santos <42756551+RicardoEstudante@users.noreply.github.com>
2020-06-06 00:39:00 -0300

    add styles.css and create-point

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 
-import Home from './pages/Home';
+import Routes from './routes';
+import { Route } from 'react-router-dom';
 
 function App(){
    return(
-     <Home />
+     <Routes />
    )
  }
 
