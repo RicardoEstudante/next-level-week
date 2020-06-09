461e8c890791ce0e5e2e56c0b85dcdc3180aefc2
Ricardo Carvalho Santos <42756551+RicardoEstudante@users.noreply.github.com>
2020-06-08 21:51:20 -0300

    add function handleSelectItem

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -32,8 +32,15 @@ const CreatePoint = () =>{
 
     const [initialPosition, setInitialPosition] = useState<[number, number]>([0,0]);
 
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        whatsapp: '',
+    })
+
     const [selectedUF, setSelectedUf] = useState('0');
     const [selectedCity, setSelectedCity] = useState('0');
+    const [selectedItems, setSelectedItems] = useState<number[]>([]);
     const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0,0]);
 
     useEffect(() => {
@@ -91,6 +98,27 @@ const CreatePoint = () =>{
         ])
     }
 
+    function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+        const { name, value } = event.target;
+
+        setFormData({...formData, [name]: value })
+    }
+
+    function hadleSelectItem(id: number) {
+        const alreadySelected = selectedItems.findIndex(item => item === id);
+
+        if(alreadySelected >= 0) {
+            const filteredItems = selectedItems.filter(item => item !== id);
+
+            setSelectedItems(filteredItems);
+        }else {
+            setSelectedItems([ ...selectedItems, id])
+        }
+    }
+
+    function handleSubmit() {
+
+    }
     return (
         <div id="page-create-point">
             <header>
@@ -116,6 +144,7 @@ const CreatePoint = () =>{
                             type="text"
                             name="name"
                             id="name"
+                            onChange={handleInputChange}
                         />
                     </div>
 
@@ -126,6 +155,7 @@ const CreatePoint = () =>{
                             type="email"
                             name="email"
                             id="email"
+                            onChange={handleInputChange}
                             />
                         </div>
                         <div className="field">
@@ -134,6 +164,7 @@ const CreatePoint = () =>{
                             type="text"
                             name="whatsapp"
                             id="whatsapp"
+                            onChange={handleInputChange}
                             />
                         </div>
                     </div>
@@ -194,7 +225,12 @@ const CreatePoint = () =>{
 
                     <ul className="items-grid">
                         {items.map(item => (
-                        <li key={item.id}>
+                        <li 
+                            key={item.id} 
+                            onClick={() => 
+                            hadleSelectItem(item.id)}
+                            className={selectedItems.includes(item.id) ? 'selected' : ''}
+                        >
                             <img src={item.image_url} alt={item.title}/>
                             <span>{item.title}</span>
                         </li>
