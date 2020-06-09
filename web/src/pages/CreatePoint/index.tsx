3f17047fff24f3c3acc798be221f55e1b2ac34ad
Ricardo Carvalho Santos <42756551+RicardoEstudante@users.noreply.github.com>
2020-06-08 22:21:46 -0300

    finish web

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
 import axios from 'axios';
@@ -42,6 +42,8 @@ const CreatePoint = () =>{
     const [selectedCity, setSelectedCity] = useState('0');
     const [selectedItems, setSelectedItems] = useState<number[]>([]);
     const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0,0]);
+
+    const history = useHistory();
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(position => {
@@ -139,6 +141,8 @@ const CreatePoint = () =>{
         await api.post('points', data);
 
         alert('Ponto de Coleta Criado');
+
+        history.push('/');
     }
 
     return (
