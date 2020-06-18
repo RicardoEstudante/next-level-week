978b61a5732e61862aaa9f3f1f3755023f2470fc
Ricardo Carvalho Santos <42756551+RicardoEstudante@users.noreply.github.com>
2020-06-18 14:50:42 -0300

    add new splash.png and background in index

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,19 +1,32 @@
 import React from 'react';
-import { View, Image, StyleSheet} from 'react-native';
+import { View, Image, ImageBackground, Text, StyleSheet } from 'react-native';
 
 
 const Home = () => {
     return( 
-    <View style={styles.container}>
+    <ImageBackground source={require('../../assets/home-background.png')} 
+    style={styles.container}
+    imageStyle={{ width: 274, height: 368 }}
+    >
+      <View style={styles.main}>
         <Image source={require('../../assets/logo.png')} />
-    </View>
+        <Text style={styles.title}>Seu Marketplace de coleta de residuos.</Text>
+        <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</Text>
+      </View>
+
+      <View style={styles.footer}>
+
+      </View>
+    </ImageBackground>
     )
+      
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 32,
+    backgroundColor: '#f0f0f5'
   },
 
   main: {
