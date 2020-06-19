014609206c3dba14af72569b6953b48271fef6fe
Ricardo Carvalho Santos <42756551+RicardoEstudante@users.noreply.github.com>
2020-06-19 11:01:32 -0300

    add btn and icon

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import { Feather as Icon} from '@expo/vector-icons';
 import { View, Image, ImageBackground, Text, StyleSheet } from 'react-native';
-
+import { RectButton } from 'react-native-gesture-handler';
 
 const Home = () => {
     return( 
@@ -15,7 +16,16 @@ const Home = () => {
       </View>
 
       <View style={styles.footer}>
-
+        <RectButton style={styles.button} onPress={() =>{}}>
+            <View style={styles.buttonIcon}> 
+            <Text>
+              <Icon name="arrow-right" color="#FFF" size={24}/>
+              </Text>
+            </View>
+            <Text style={styles.buttonText}>
+              Entrar
+            </Text>
+          </RectButton>
       </View>
     </ImageBackground>
     )
